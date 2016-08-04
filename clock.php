865d30e5dbadd11ed4e74f50324ebd8c396b98f1
Vladimir Kamuz <v.kamuz@gmail.com>
2016-08-04 20:26:44 +0300

    Add main files

---
+++ clock.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Clock PHP</title>
+  <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+</head>
+<body>
+  <p id="time"></p>
+  <a href="index.php">Go back</a>
+  <script>
+    function show(){
+      $.ajax({
+        url: "time.php",
+        cache: false,
+        success: function(html){
+          $('#time').html(html)
+        }
+      })
+    }
+    setInterval('show()', 1000);
+  </script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Index</title>
+</head>
+<body>
+  <h1>Lorem ipsum dolor sit amet.</h1>
+  <a href="clock.php">Get time</a>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non eligendi maiores mollitia adipisci recusandae ratione, cupiditate aliquam distinctio consequatur id dolorum voluptatibus odio harum animi minus nulla nostrum dolore doloremque.</p>
+  <p>Nobis rem ipsum similique cumque aperiam, itaque, numquam qui expedita quisquam magni nisi eaque dolores. Ab dolorem nulla maiores laboriosam porro suscipit quasi! Officiis, possimus dicta quasi inventore earum dignissimos!</p>
+  <p>Quidem quos corrupti voluptatum ab tenetur assumenda iure, ipsa sit architecto eos mollitia hic, fugiat rem aliquam deleniti. Sint totam dignissimos sequi voluptas debitis numquam consectetur quod unde autem consequuntur.</p>
+  <p>Iste nisi consequatur ipsum fuga reiciendis possimus quia quidem eaque illo quibusdam est, a, nam eos laboriosam numquam laudantium nihil! Omnis ex corporis voluptatum sunt, sint nobis error culpa placeat.</p>
+</body>
+</html>
